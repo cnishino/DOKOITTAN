3abed4a0afb5ff4949da781683eb20407a07e8c8
cnishino <chihironisino@gmail.com>
2022-04-05 12:20:37 +0000

    [add]投稿機能

--- app/controllers/public/post_locations_controller.rb
+++ app/controllers/public/post_locations_controller.rb
@@ -1,19 +1,63 @@
 class Public::PostLocationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
+  def show
+    @post_location = PostLocation.find(params[:id])
+  end
+
   def index
+    @post_locations = PostLocation.where(is_active: "true" )#.page(params[:page]).per(8)
+    # @genres = Genre.all
+    # @prefectures = Prefecture.all
   end
 
   def create
-  end
-
-  def show
+    @post_location = PostLocation.new(post_location_params)
+    @post_location.user_id = current_user.id
+    #@post_location.is_active == true
+    if @post_location.save
+      redirect_to post_location_path(@post_location)
+      flash[:notice] = "投稿しました。"
+    else
+      @post_locations = PostLocation.all
+      render 'index'
+      flash[:alert] = "全ての項目を入力してください。"
+    end
   end
 
   def edit
   end
 
   def update
+    @post_location = PostLocation.find(params[:id])
+    if @post_location.update(post_location_params)
+      redirect_to post_location_path(@post_location)
+      flash[:notice] = "投稿を編集しました。"
+    else
+      render "edit"
+      flash[:alert] = "全ての項目を入力してください。"
+    end
   end
 
   def destroy
+    @post_location = PostLocation.find(params[:id])
+    @post_location.destroy
+    redirect_to post_locations_path
+    flash[:alert] = "投稿を削除しました。"
+  end
+
+  private
+
+  def post_location_params
+    params.require(:post_location).permit(:user_id, :genre_id, :prefecture_id, :facility_name, :target_age, :introduction, :review, :is_active)
+  end
+
+  def ensure_correct_user
+    @post_location = PostLocation.find(params[:id])
+    unless @post_location.user == current_user
+      redirect_to post_locations_path
+    end
   end
 end
+
