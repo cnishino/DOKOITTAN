f7ad0b7035a9e29b91fde11a304d1f1a6a6622f1
cnishino <cnishino19820915@icloud.com>
2022-05-17 01:25:43 +0000

    [fix]無限スクロール

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
@@ -16,6 +17,6 @@ window.$ = window.jQuery = require('jquery');
 require('packs/raty')
 
 Rails.start()
-// Turbolinks.start()
+Turbolinks.start()
 ActiveStorage.start()
 
